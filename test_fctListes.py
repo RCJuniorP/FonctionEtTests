bd573af2bc1fa21457c25f28998c132e71e74597
RCJuniorP <2541215@cegepoutaouais.qc.ca>
2025-10-03 12:26:02 -0400

    Fini les test unitaires de la fonction 3.

--- test_fctListes.py
+++ test_fctListes.py
@@ -8,13 +8,15 @@ from fctListes import fonction1, fonction2, moyenne_par_couleur
 @pytest.mark.parametrize("liste_lego, resultat_attendu", [
     (["rouge", "bleu", "jaune"], 1.00),
     (["rouge", "rouge", "bleu", "bleu", "jaune", "jaune"], 2.00),
-    ([], 0.00),
-    (["rouge", "bleu", "jaune", "jaune"], 1.33),
+    ([], 0.00), # Les listes vides devraient retourner 0
+    (["rouge", "bleu", "jaune", "jaune"], 1.33),  # On doit arrondir à deux chiffres
+    (["rouge", "rouge", "bleu", "bleu", "JAUNE", "jaune"], 2.00),  # Les majuscules devraient être ignorées
+    (["rouge", "bleu", "vert", "vert", "jaune", "jaune"], 1.50),
 ])
 # Act
 def test_fonction3(liste_lego, resultat_attendu):
     resultat_obtenu = moyenne_par_couleur(liste_lego)
 
     # Assert
-    assert type(resultat_obtenu) == float
+    assert isinstance(resultat_obtenu, float)
     assert resultat_attendu == resultat_obtenu
